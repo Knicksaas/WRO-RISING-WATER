8cec5f3cdb0ae713d69f9e34c536793df3080685
Niklas <niklas.sidler@gmail.com>
2020-02-16 16:12:09 +0100

    unnessesary commit xD

--- WRO-2020/src/ch/nte/wro/main/MovmentBlocks.java
+++ WRO-2020/src/ch/nte/wro/main/MovmentBlocks.java
@@ -9,7 +9,7 @@ public class MovmentBlocks {
 	}
 	public static void driveToHouse(Robot bot, int speed, String mode) {
 	float sensitivity2 = 0;	
-	float sensitivity1;
+	float sensitivity1 = 0;
 	//if for sensitivity 
 	bot.followLineRGB(speed, "double.cross", 0, sensitivity1, bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
 	bot.followLineRGB(speed, "double.cross", 0, sensitivity2, bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
