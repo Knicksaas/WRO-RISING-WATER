4591c6235b6ebb00db0cb3e42d663d89aa14643a
Nickitiki <46298023+Nickitiki@users.noreply.github.com>
2020-02-16 18:33:37 +0100

    changeSide finished, startPointToHouse finished

--- WRO-2020/src/ch/nte/wro/main/MovmentBlocks.java
+++ WRO-2020/src/ch/nte/wro/main/MovmentBlocks.java
@@ -28,10 +28,48 @@ public class MovmentBlocks {
 		Delay.msDelay(100);
 		bot.accelerate(speed*2, speed, 1000);
 		bot.rotate(speed*2, 1F);
+		startPointToLine(bot, speed, side2);
+		if(Position.botPosition.equalsIgnoreCase("red") || Position.botPosition.equalsIgnoreCase("yellow")) {
+			if(side2.equalsIgnoreCase("right")) {
+				Position.botPosition = "blue";
+			} else {
+				Position.botPosition = "green";
+			}
+		} else {
+			if(side2.equalsIgnoreCase("right")) {
+				Position.botPosition = "red";
+			} else {
+				Position.botPosition = "yellow";
+			}
+		}
+	}
+	
+	public static void driveToHouse(Robot bot, int speed, String mode) {
+	//if for sensitivity 
+	bot.followLineRGB(speed, "double.cross", 0, SensorValues.sensitivity.get(Position.botPosition), bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
+	bot.followLineRGB(speed, "double.cross", 0, SensorValues.sensitivity.get(Position.botPosition), bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
+	//if for mode
+	}
+	
+	public static void pickUpSandBags(Robot bot, int speed, String side) {
+		bot.followLineRGB(speed, "double.cross", 0, SensorValues.sensitivity.get(Position.botPosition),
+				bot.getSensorOnPort(1), bot.getSensorOnPort(2));
+		bot.rotate(speed, 0.3F);
+		if(side.equalsIgnoreCase("left")) {
+			bot.rotate(speed*2, 1f, "right");
+		} else if (side.equalsIgnoreCase("right")) {
+			bot.rotate(speed*2, 1f, "left");
+		}
+		bot.followLineRGB(speed, "double.cross", 0, 40, bot.getSensorOnPort(1), bot.getSensorOnPort(2));
+		bot.sandBagPickUp(speed, bot.getSensorOnPort(3));
+		bot.turnWithRotations(speed, 1f, side);
+	}
+	
+	public static void startPointToLine(Robot bot, int speed, String side) {
 		bot.forwardUntil(speed, bot.getSensorOnPort(1), bot.getSensorOnPort(2),
 				SensorValues.intensityWhite, SensorValues.allowedSensorVariation*3);
 		bot.rotate(speed, 0.5f);
-		if(side2.equalsIgnoreCase("left")) {
+		if(side.equalsIgnoreCase("left")) {
 			bot.turnToLine(speed, "double.left", bot.getSensorOnPort(1), SensorValues.intensityWhite, SensorValues.allowedSensorVariation*2);
 			new BeepThread().start();
 			bot.turnWithRotations(speed, 0.2f, "left");
@@ -40,12 +78,6 @@ public class MovmentBlocks {
 			new BeepThread().start();
 			bot.turnWithRotations(speed, 0.2f, "right");
 		}
-	}
-	public static void driveToHouse(Robot bot, int speed, String mode) {
-	//if for sensitivity 
-	bot.followLineRGB(speed, "double.cross", 0, SensorValues.sensitivity.get(Position.botPosition), bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
-	bot.followLineRGB(speed, "double.cross", 0, SensorValues.sensitivity.get(Position.botPosition), bot.getSensorOnPort(1), bot.getSensorOnPort(2));	
-	//if for mode
 	}
 }
 
