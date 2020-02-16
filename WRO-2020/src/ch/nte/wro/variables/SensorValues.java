ae0d4b08582b3200b0383b99b582b2cda7012628
Niklas <niklas.sidler@gmail.com>
2020-02-16 12:57:08 +0100

    rgb linefolower tested

--- WRO-2020/src/ch/nte/wro/variables/SensorValues.java
+++ WRO-2020/src/ch/nte/wro/variables/SensorValues.java
@@ -6,20 +6,20 @@ public class SensorValues {
 	
 //	NONE, BLACK, BLUE, GREEN, YELLOW, RED, WHITE, BROWN: colorIDs of Ev3ColorSensor (0-7)
 	
-	public static float intensityBlack = 0.07F;
-	public static float intensityWhite = 0.559F;
-	public static float intensityBlue = 0.080F;
-	public static float intensityGreen = 0.117F;
-	public static float intensityYellow = 0.52F;
-	public static float intensityRed = 0.484F;
+	public static float intensityBlack = 0.061F;
+	public static float intensityWhite = 0.542F;
+	public static float intensityBlue = 0.087F;
+	public static float intensityGreen = 0.114F;
+	public static float intensityYellow = 0.525F;
+	public static float intensityRed = 0.455F;
 	
-	public static RGBValue valueBlack = new RGBValue(0, 0, 0);
+	public static RGBValue valueBlack = new RGBValue(0.02245f, 0.025f, 0.0268f);
 	
 	public static float targetIntensityLinefollower = (intensityBlack+intensityWhite)/2;
 	
 	public static float averageIntensityHalfCross = 0F;
 	
 	public static float allowedSensorVariation = 0.01F;
-	public static float allowedRGBVariation = 0f;
+	public static float allowedRGBVariation = 0.03f;
 
 }
